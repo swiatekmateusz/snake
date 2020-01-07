6f7927cc1f5c6acb65fe700a2278724d5cc4d780
swiatekmateusz <swiatek.mateusz.m@gmail.com>
2020-01-07 20:04:53 +0100

    Features

--- snake.js
+++ snake.js
@@ -2,8 +2,8 @@
 const areaX = 20;
 const areaY = 20;
 const snakeItemWH = 20;
-const snakeStartX = Math.floor(areaX/2)
-const snakeStartY = Math.floor(areaY/2)
+const snakeStartX = Math.floor(areaX / 2)
+const snakeStartY = Math.floor(areaY / 2)
 const frequency = 250
 
 //Program variables
@@ -12,9 +12,10 @@ const lose = document.querySelector('.lose')
 const container = document.querySelector('.snake-container')
 const score = document.getElementById('score')
 const btnReset = document.getElementById('restart')
-let snakeX = snakeStartX 
-let snakeY = snakeStartY 
-let snakePositions = [[snakeX,snakeY]]
+let snakeX = snakeStartX
+let snakeY = snakeStartY
+let snakePositions = [[snakeX, snakeY]]
+let directions = []
 let pointX;
 let pointY;
 let head;
@@ -24,14 +25,14 @@ score.textContent = scoreCounter
 let over = false;
 let keydownFlag = false
 let firstRender = true;
-const generateArea = ()=>{  
-  container.style.width = `${snakeItemWH*areaX}px`
-  container.style.height = `${snakeItemWH*areaY}px`
-  for(let r=0;r<areaX;r++){
+const generateArea = () => {
+  container.style.width = `${snakeItemWH * areaX}px`
+  container.style.height = `${snakeItemWH * areaY}px`
+  for (let r = 0; r < areaX; r++) {
     const row = document.createElement('div');;
     row.classList.add('snake-row')
     areaTable.push([])
-    for(let t=0;t<areaY;t++){
+    for (let t = 0; t < areaY; t++) {
       const rowItem = document.createElement('div');;
       rowItem.classList.add('snake-item')
       rowItem.style.width = `${snakeItemWH}px`
@@ -40,21 +41,80 @@ const generateArea = ()=>{
       row.appendChild(rowItem)
       areaTable[r][t] = rowItem
     }
-  container.appendChild(row)
+    container.appendChild(row)
   }
 }
 
-const clearSnake = () =>{
-  snakePositions.forEach(pos=>{
-    if(pos==snakePositions[0])  areaTable[pos[0]][pos[1]].classList.remove('snake-head','dol','prawo','lewo','gora','undefined')
-    else areaTable[pos[0]][pos[1]].classList.remove('snake-body')
+const clearSnake = () => {
+  snakePositions.forEach(pos => {
+    if (pos == snakePositions[0]) areaTable[pos[0]][pos[1]].classList.remove('snake-head', 'dol', 'prawo', 'lewo', 'gora', 'undefined')
+    else areaTable[pos[0]][pos[1]].classList.remove('snake-body', 'prosto-x', 'prosto-y', 'dol-prawo', 'gora-prawo', 'gora-lewo', 'dol-lewo', 'snake-tail', 'snake-tail-dol', 'snake-tail-gora', 'snake-tail-prawo', 'snake-tail-lewo')
   })
 }
 
-const drawSnake = () =>{
-  snakePositions.forEach(pos=>{
-    if(pos==snakePositions[0])  areaTable[pos[0]][pos[1]].classList.add('snake-head')
-    else  areaTable[pos[0]][pos[1]].classList.add('snake-body')
+const drawSnake = () => {
+  snakePositions.forEach((pos, i) => {
+    if (pos == snakePositions[0]) areaTable[pos[0]][pos[1]].classList.add('snake-head')
+    else {
+      if (snakePositions.length > 2 && pos !== snakePositions[snakePositions.length - 1]) {
+
+        if (((snakePositions[i - 1][0] === pos[0] && snakePositions[i - 1][1] === (pos[1] + 1) % (areaY) && snakePositions[i + 1][0] === pos[0] && snakePositions[i + 1][1] === (pos[1] - 1) % (areaY)) ||
+          (snakePositions[i - 1][0] === pos[0] && snakePositions[i - 1][1] === (pos[1] - 1) % (areaY) && snakePositions[i + 1][0] === pos[0] && snakePositions[i + 1][1] === (pos[1] + 1) % (areaY))) ||
+          ((snakePositions[i - 1][0] === pos[0] && snakePositions[i - 1][1] === 1 && snakePositions[i + 1][0] === pos[0] && snakePositions[i + 1][1] === 19) ||
+            (snakePositions[i - 1][0] === pos[0] && snakePositions[i - 1][1] === 19 && snakePositions[i + 1][0] === pos[0] && snakePositions[i + 1][1] === 1))
+        ) {
+          areaTable[pos[0]][pos[1]].classList.add('prosto-y')
+        } else if (((snakePositions[i - 1][0] - 1 === pos[0] && snakePositions[i - 1][1] === pos[1] && snakePositions[i + 1][0] + 1 === pos[0] && snakePositions[i + 1][1] === pos[1]) ||
+          (snakePositions[i - 1][0] + 1 === pos[0] && snakePositions[i - 1][1] === pos[1] && snakePositions[i + 1][0] - 1 === pos[0] && snakePositions[i + 1][1] === pos[1]))
+          ||
+          ((snakePositions[i - 1][0] === 1 && snakePositions[i - 1][1] === pos[0] && snakePositions[i + 1][0] === 19 && snakePositions[i + 1][1] === pos[0]) ||
+            (snakePositions[i - 1][0] === 19 && snakePositions[i - 1][1] === pos[0] && snakePositions[i + 1][0] === 1 && snakePositions[i + 1][1] === pos[0]))) {
+          areaTable[pos[0]][pos[1]].classList.add('prosto-x')
+        } else if ((snakePositions[i - 1][0] === pos[0] && snakePositions[i - 1][1] + 1 === pos[1] && snakePositions[i + 1][0] - 1 === pos[0] && snakePositions[i + 1][1] === pos[1]) ||
+          (snakePositions[i - 1][0] - 1 === pos[0] && snakePositions[i - 1][1] === pos[1] && snakePositions[i + 1][0] === pos[0] && snakePositions[i + 1][1] + 1 === pos[1])) {
+          areaTable[pos[0]][pos[1]].classList.add('dol-prawo')
+        } else if ((snakePositions[i - 1][0] === pos[0] && snakePositions[i - 1][1] - 1 === pos[1] && snakePositions[i + 1][0] - 1 === pos[0] && snakePositions[i + 1][1] === pos[1]) ||
+          (snakePositions[i - 1][0] - 1 === pos[0] && snakePositions[i - 1][1] === pos[1] && snakePositions[i + 1][0] === pos[0] && snakePositions[i + 1][1] - 1 === pos[1])) {
+          areaTable[pos[0]][pos[1]].classList.add('gora-prawo')
+        } else if ((snakePositions[i - 1][0] === pos[0] && snakePositions[i - 1][1] - 1 === pos[1] && snakePositions[i + 1][0] + 1 === pos[0] && snakePositions[i + 1][1] === pos[1]) ||
+          (snakePositions[i - 1][0] + 1 === pos[0] && snakePositions[i - 1][1] === pos[1] && snakePositions[i + 1][0] === pos[0] && snakePositions[i + 1][1] - 1 === pos[1])) {
+          areaTable[pos[0]][pos[1]].classList.add('gora-lewo')
+        } else if ((snakePositions[i - 1][0] === pos[0] && snakePositions[i - 1][1] + 1 === pos[1] && snakePositions[i + 1][0] + 1 === pos[0] && snakePositions[i + 1][1] === pos[1]) ||
+          (snakePositions[i - 1][0] + 1 === pos[0] && snakePositions[i - 1][1] === pos[1] && snakePositions[i + 1][0] === pos[0] && snakePositions[i + 1][1] + 1 === pos[1])) {
+          areaTable[pos[0]][pos[1]].classList.add('dol-lewo')
+        }
+        const last = snakePositions[snakePositions.length - 1]
+        const second = snakePositions[snakePositions.length - 2]
+        if (last[0] === second[0] && last[1] - 1 === second[1]) {
+          areaTable[last[0]][last[1]].classList.add('snake-tail-gora')
+        } else if (last[0] === second[0] && last[1] + 1 === second[1]) {
+          areaTable[last[0]][last[1]].classList.add('snake-tail-dol')
+        } else if (last[0] + 1 === second[0] && last[1] === second[1]) {
+          areaTable[last[0]][last[1]].classList.add('snake-tail-prawo')
+        } else if (last[0] - 1 === second[0] && last[1] === second[1]) {
+          areaTable[last[0]][last[1]].classList.add('snake-tail-lewo')
+        }
+      } else if (snakePositions.length === 2) {
+        const pos = snakePositions[1]
+        switch (direction) {
+          case 'gora':
+            areaTable[pos[0]][pos[1]].classList.add('snake-tail-gora')
+            break;
+          case 'dol':
+            areaTable[pos[0]][pos[1]].classList.add('snake-tail-dol')
+            break;
+          case 'lewo':
+            areaTable[pos[0]][pos[1]].classList.add('snake-tail-lewo')
+            break;
+          case 'prawo':
+            areaTable[pos[0]][pos[1]].classList.add('snake-tail-prawo')
+            break;
+        }
+
+      }
+      areaTable[pos[0]][pos[1]].classList.add('snake-body')
+
+    }
   })
   head = document.querySelector('.snake-head')
   head.classList.add(direction)
@@ -62,36 +122,41 @@ const drawSnake = () =>{
 
 const snakeMove = () => {
   clearSnake()
-  snakePositions.pop()
+
   switch (direction) {
     case 'gora':
-    snakeY = snakeY -1 < 0 ? areaY-1 :snakeY-1;
-    break;
+      snakeY = snakeY - 1 < 0 ? over = true : snakeY - 1;
+      break;
     case 'dol':
-    snakeY = (snakeY +1)%areaY;
-    break;
+      snakeY = (snakeY + 1) > 19 ? over = true : (snakeY + 1);
+      break;
     case 'lewo':
-    snakeX = snakeX -1 < 0 ? areaX-1 : snakeX-1;
-    break;
+      snakeX = snakeX - 1 < 0 ? over = true : snakeX - 1;
+      break;
     case 'prawo':
-    snakeX = (snakeX +1)%areaX;
-    break;
+      snakeX = (snakeX + 1) > 19 ? over = true : (snakeX + 1);
+      break;
   }
-  snakePositions.unshift([snakeX,snakeY])
+  if (over) {
+    drawSnake()
+    return null
+  }
+  snakePositions.pop()
+  snakePositions.unshift([snakeX, snakeY])
   drawSnake()
 }
 
-const drawPoint = () =>{
+const drawPoint = () => {
   try {
-  document.querySelector('.point').classList.remove('point')
+    document.querySelector('.point').classList.remove('point')
   }
-  catch(TypeError){}
+  catch (TypeError) { }
 
-  pointX = Math.floor(Math.random()*areaX)
-  pointY = Math.floor(Math.random()*areaY)
+  pointX = Math.floor(Math.random() * areaX)
+  pointY = Math.floor(Math.random() * areaY)
 
-  for(let pos of snakePositions){
-    if(pointX === pos[0] && pointY === pos[1]){
+  for (let pos of snakePositions) {
+    if (pointX === pos[0] && pointY === pos[1]) {
       drawPoint()
       break
     }
@@ -99,78 +164,81 @@ const drawPoint = () =>{
   areaTable[pointX][pointY].classList.add('point')
 }
 
-const eat = () =>{
-  if(snakeX === pointX && snakeY === pointY){
-    snakePositions.unshift([pointX,pointY])
-;
+const eat = () => {
+  if (snakeX === pointX && snakeY === pointY) {
+    snakePositions.push([pointX, pointY])
     score.textContent = ++scoreCounter;
     drawPoint()
   }
 }
 
-const GameOver = () =>{
-  snakePositions.slice(4).forEach(pos=>{
-    if(pos[0] == snakeX && pos[1] == snakeY){
+const GameOver = () => {
+  snakePositions.slice(4).forEach(pos => {
+    if ((pos[0] == snakeX && pos[1] == snakeY) || (pos[0] < 0 || pos[0] > 19) || (pos[1] < 0 || pos[1] > 19)) {
       over = true
-      lose.classList.add('active')
-   }
+    }
   })
 
 }
 
-btnReset.addEventListener('click',() =>{
+btnReset.addEventListener('click', () => {
   clearInterval(GameInterval)
   lose.classList.remove('active')
   container.innerHTML = ''
-  snakeX = snakeStartX 
-  snakeY = snakeStartY 
-  snakePositions = [[snakeX,snakeY]]
+  snakeX = snakeStartX
+  snakeY = snakeStartY
+  snakePositions = [[snakeX, snakeY]]
   score.textContent = 0;
   scoreCounter = 0
-  score.textContent= scoreCounter
-  direction =  undefined;
+  score.textContent = scoreCounter
+  direction = undefined;
   over = false;
   firstRender = true;
-  GameInterval = setInterval(Game,frequency)
+  GameInterval = setInterval(Game, frequency)
 })
 
-window.addEventListener('keydown',e=>{
-  if(!keydownFlag){
-  switch (e.keyCode) {
+window.addEventListener('keydown', e => {
+  if (!keydownFlag) {
+    switch (e.keyCode) {
       case 38:
       case 87:
-      if(direction !== 'dol')direction = 'gora'
-      break;
+        if (direction !== 'dol') direction = 'gora'
+        break;
       case 40:
       case 83:
-      if(direction !== 'gora') direction = 'dol'
-      break;
+        if (direction !== 'gora') direction = 'dol'
+        break;
       case 37:
       case 65:
-      if(direction !== 'prawo')direction = 'lewo'
-      break;
+        if (direction !== 'prawo') direction = 'lewo'
+        break;
       case 39:
       case 68:
-      if(direction !== 'lewo')direction = 'prawo'
-      break;
-  }
-  keydownFlag = true;
+        if (direction !== 'lewo') direction = 'prawo'
+        break;
+
+    }
+    directions[0] = direction
+    keydownFlag = true;
   }
 })
 
 
-const Game = () =>{
-  if(firstRender){
+const Game = () => {
+  if (firstRender) {
     generateArea()
     drawPoint()
     firstRender = false;
   }
-  if(over){
+  if (over) {
     clearInterval(GameInterval)
     return;
   }
   snakeMove()
   GameOver()
+  if (over) {
+    lose.classList.add('active')
+  }
   eat()
   keydownFlag = false;
 }
@@ -178,7 +246,7 @@ const Game = () =>{
 const switching = document.querySelector('.switch')
 
 const whiteBtn = document.getElementById('white')
-whiteBtn.addEventListener('click',()=>{
+whiteBtn.addEventListener('click', () => {
   document.body.style.backgroundColor = "white"
   container.style.borderColor = "black"
   document.body.style.color = "black"
@@ -186,11 +254,11 @@ whiteBtn.addEventListener('click',()=>{
 })
 
 const blackBtn = document.getElementById('black')
-blackBtn.addEventListener('click',()=>{
+blackBtn.addEventListener('click', () => {
   document.body.style.backgroundColor = "black"
   container.style.borderColor = "white"
   document.body.style.color = "white"
   switching.style.boxShadow = "0 0 0 5px white"
 })
 
-let GameInterval = setInterval(Game,frequency)
+let GameInterval = setInterval(Game, frequency)
